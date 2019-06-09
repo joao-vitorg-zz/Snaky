c34af3724173bf93122024bd0bfd6c4c75c2a71a
joao <jovitor737@gmail.com>
2019-06-09 14:31:04 -0300

    add Food

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from curses import KEY_RIGHT, KEY_LEFT, KEY_DOWN, KEY_UP
+from curses import KEY_RIGHT, KEY_LEFT, KEY_DOWN, KEY_UP, color_pair
+from random import randint
 
 # Configuration
 HEIGHT = 20
@@ -13,3 +14,21 @@ REV_DIR_MAP = {KEY_RIGHT: KEY_LEFT,
                KEY_LEFT: KEY_RIGHT,
                KEY_DOWN: KEY_UP,
                KEY_UP: KEY_DOWN}
+
+
+class Food:
+    def __init__(self, window):
+        self.x = randint(1, MAX_X)
+        self.y = randint(1, MAX_Y)
+        self.window = window
+
+    def render(self):
+        self.window.addstr(self.y, self.x, "█", color_pair(3))
+
+    def renew(self):
+        self.x = randint(1, MAX_X)
+        self.y = randint(1, MAX_Y)
+
+    @property
+    def coor(self):
+        return self.x, self.y
