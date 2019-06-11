47a7e890e22874caaa2f1719972c2a3c8d5d5ec1
joao <jovitor737@gmail.com>
2019-06-11 18:47:13 -0300

    reformat main.py

--- main.py
+++ main.py
@@ -1,163 +1,63 @@
-from curses import KEY_RIGHT, KEY_LEFT, KEY_DOWN, KEY_UP, color_pair, endwin, newwin, start_color, initscr, noecho, \
-    curs_set, init_pair
-from random import randint
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from curses import KEY_RIGHT, KEY_LEFT, KEY_DOWN, KEY_UP, endwin, newwin, start_color, initscr, noecho, curs_set, \
+    init_pair
 
-# Configuration
-HEIGHT = 20
-WIDTH = 35
-MAX_Y = HEIGHT - 2
-MAX_X = WIDTH - 2
-TIMEOUT = 100
-SNAKE_LENGTH = 6
-SNAKE_X = SNAKE_LENGTH + 1
-SNAKE_Y = 3
-REV_DIR_MAP = {KEY_RIGHT: KEY_LEFT,
-               KEY_LEFT: KEY_RIGHT,
-               KEY_DOWN: KEY_UP,
-               KEY_UP: KEY_DOWN}
+from src.conf import HEIGHT, WIDTH, TIMEOUT
+from src.widgets import *
 
 
-class Food:
-    def __init__(self, window):
-        self.coor = (randint(1, MAX_Y), randint(1, MAX_X))
-        self.window = window
-
-    def render(self):
-        self.window.addstr(*self.coor, "█", color_pair(3))
-
-    def renew(self):
-        self.coor = (randint(1, MAX_Y), randint(1, MAX_X))
-
-
-class Body:
-    def __init__(self, y, x, window):
-        self.window = window
-        self.body_list = []
-
-        # Head
-        self.last_head_coor = (y, x)
-        self.head_y = y
-        self.head_x = x
-
-    def render(self):
-        self.window.addstr(*self.head_coor, '█', color_pair(2))  # Head
-        for body in self.body_list:
-            self.window.addstr(*body, '█', color_pair(1))  # Body
-
-    def add_body(self, y, x):
-        self.body_list.append([y, x])
-
-    @property
-    def head_coor(self):
-        return self.head_y, self.head_x
-
-
-class Snake(Body):
-    def __init__(self, y, x, window):
-        super().__init__(y, x, window)
-        self.direction = KEY_RIGHT
-        self.timeout = TIMEOUT
-        self.hit_score = 0
-        self.direction_map = {KEY_RIGHT: self.move_right,
-                              KEY_LEFT: self.move_left,
-                              KEY_DOWN: self.move_down,
-                              KEY_UP: self.move_up}
-
-        for i in range(1, SNAKE_LENGTH):
-            self.add_body(y, x-i)
-
-    def update(self):
-        self.body_list.pop(-1)
-        self.body_list.insert(0, self.head_coor)
-        self.last_head_coor = self.head_coor
-        self.direction_map[self.direction]()
-
-        return any([tuple(body) == self.head_coor for body in self.body_list])
-
-    def eat_food(self):
-        self.add_body(*self.last_head_coor)
-        self.hit_score += 1
-        if self.hit_score % 3 == 0:
-            self.timeout -= 5
-            self.window.timeout(self.timeout)
-
-    def change_direction(self, direction):
-        if direction != REV_DIR_MAP[self.direction]:
-            self.direction = direction
-
-    def move_up(self):
-        self.head_y -= 1
-        if self.head_y < 1:
-            self.head_y = MAX_Y
-
-    def move_down(self):
-        self.head_y += 1
-        if self.head_y > MAX_Y:
-            self.head_y = 1
-
-    def move_left(self):
-        self.head_x -= 1
-        if self.head_x < 1:
-            self.head_x = MAX_X
-
-    def move_right(self):
-        self.head_x += 1
-        if self.head_x > MAX_X:
-            self.head_x = 1
-
-    @property
-    def score(self):
-        return 'Score: {}'.format(self.hit_score)
-
-
-if __name__ == '__main__':
-    try:
+class SnakyGame:
+    def __init__(self):
         initscr()
         start_color()
         curs_set(0)
         noecho()
 
-        init_pair(1, 2, 0)  # Green
-        init_pair(2, 3, 0)  # yellow
-        init_pair(3, 1, 0)  # Red
+        init_pair(1, 2, 0)
+        init_pair(2, 3, 0)
+        init_pair(3, 1, 0)
 
-        window = newwin(HEIGHT, WIDTH, 0, 0)
-        window.timeout(TIMEOUT)
-        window.keypad(1)
-        window.border(0)
+        self.window = newwin(HEIGHT, WIDTH, 0, 0)
+        self.window.timeout(TIMEOUT)
+        self.window.keypad(1)
+        self.window.border(0)
 
-        snake = Snake(SNAKE_Y, SNAKE_X, window)
-        food = Food(window)
+        self.snake = Snake(self.window)
+        self.food = Food(self.window)
 
-        while True:
-            window.clear()
-            window.border(0)
-            snake.render()
-            food.render()
+        self.main_loop()
 
-            # window.addstr(0, 0, str([snake.head_coor]+[body for body in snake.body_list]))
-            window.addstr(0, 13, snake.score)
+    def main_loop(self):
+        try:
+            while True:
+                self.window.clear()
+                self.window.border(0)
+                self.snake.render()
+                self.food.render()
 
-            event = window.getch()
+                self.window.addstr(0, 13, self.snake.score)
 
-            if event in [KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT]:
-                snake.change_direction(event)
+                event = self.window.getch()
 
-            if snake.head_coor == food.coor:
-                food.renew()
-                snake.eat_food()
+                if event in [KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT]:
+                    self.snake.change_direction(event)
+                if self.snake.head_coor == self.food.coor:
+                    self.food.renew()
+                    self.snake.eat_food()
+                if event == 32:
+                    key = -1
+                    while key != 32:
+                        key = self.window.getch()
+                if self.snake.update():
+                    break
+            endwin()
+            print(self.snake.score)
 
-            if event == 32:
-                key = -1
-                while key != 32:
-                    key = window.getch()
+        except (KeyboardInterrupt, Exception) as e:
+            endwin()
+            print('\033[93m', e, '\033[0m \n\n')
 
-            if snake.update():
-                break
 
-        endwin()
-        print(snake.score)
-
-    except (KeyboardInterrupt, Exception) as e:
-        endwin()
-        print('\033[93m', e, '\033[0m \n')
+if __name__ == '__main__':
+    SnakyGame()
