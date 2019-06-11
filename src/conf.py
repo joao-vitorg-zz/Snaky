1771175d8d271fc43d92f71f0fadf5fec6c4facd
joao <jovitor737@gmail.com>
2019-06-11 18:25:37 -0300

    add conf file

---
+++ src/conf.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__all__ = ['HEIGHT', 'WIDTH', 'MAX_X', 'MAX_Y', 'TIMEOUT', 'SNAKE_LENGTH', 'SNAKE_X', 'SNAKE_Y']
+
+HEIGHT = 20
+WIDTH = 35
+MAX_Y = HEIGHT - 2
+MAX_X = WIDTH - 2
+TIMEOUT = 100
+SNAKE_LENGTH = 6
+SNAKE_X = SNAKE_LENGTH + 1
+SNAKE_Y = 3
